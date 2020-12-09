234acef0e73e388f8ff9cf7a9090a9ecb28ec796
RODRIGO VIEIRA BARBOSA DA SILVA <rodrigo.silva@midway.com.br>
2020-12-09 05:02:22 -0300

    Projeto company iniciado

---
+++ src/main/java/br/com/vieirarodrigo/company/CompanyApplication.java
@@ -0,0 +1,13 @@
+package br.com.vieirarodrigo.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vieirarodrigo/company/CompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vieirarodrigo.company;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
